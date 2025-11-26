729499187b6f7aabcb047dc1f4ffce3bb4638263
anbepa <andro11anto@gmail.com>
2025-11-25 21:28:11 -0500

    Remove animations dependency and provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 // Importa 'withFetch' junto con provideHttpClient y withInterceptorsFromDi
 import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -10,9 +9,7 @@ export const appConfig: ApplicationConfig = {
     // Configura provideHttpClient para usar withInterceptorsFromDi y AHORA también withFetch()
     provideHttpClient(withInterceptorsFromDi(), withFetch()),
     // Configura el enrutador
-    provideRouter(routes),
-    // Configura animaciones asíncronas para ToastComponent
-    provideAnimationsAsync()
+    provideRouter(routes)
     // provideHttpClient() puede recibir varias opciones, withInterceptorsFromDi() y withFetch() son dos de ellas
   ]
 };
